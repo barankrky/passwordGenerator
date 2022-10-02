0b74fe5179bec1845469f31352b266ce1d7961a9
Baran Karakaya <53481486+barankrky@users.noreply.github.com>
2022-10-02 20:42:01 +0300

    code cleanup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import ui_main, sys, random, string, pyperclip
+from string import *
+import ui_main, sys, random, pyperclip
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,26 +10,18 @@ class MainWindow(QMainWindow):
         self.main.setupUi(self)
         self.main.generateButton.clicked.connect(lambda: self.generate_password())
         self.generate_password()
-        
-        
+
     def generate_password(self):
         length = 16
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        num = string.digits
-        symbols = string.punctuation
-        all = lower + lower + upper + upper + num + symbols
-        temp = random.sample(all, length)
+        rndstr = (
+            ascii_lowercase + ascii_lowercase + ascii_uppercase + digits + punctuation
+        )
+        temp = random.sample(rndstr, length)
         password = "".join(temp)
         self.main.passOutput.setText(password)
         self.main.passOutput.selectAll()
         pyperclip.copy(password)
 
-        
-        
-        
-        
-    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
